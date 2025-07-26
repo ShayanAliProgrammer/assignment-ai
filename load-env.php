217077a943b0284fc87b0a36a1ea8bc54c791a45
ShayanAliProgrammer <janishayan10@gmail.com>
2025-07-26 09:31:03 +0500

    update: load-env.php

--- load-env.php
+++ load-env.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+// require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createMutable(__DIR__);
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createMutable(__DIR__);
+// $dotenv->load();
+
+$_ENV['GEMINI_API_KEY'] = getenv('GEMINI_API_KEY');
